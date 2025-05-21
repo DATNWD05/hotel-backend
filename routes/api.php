e1c8fdf29a022d48a001805ddc0c7d8e0f0e46c2
Thang-day <thangtmph49748@gmail.com>
2025-05-21 13:04:49 +0700

    test

---
+++ routes/api.php
@@ -0,0 +1,20 @@
+<?php
+
+use App\Http\Controllers\Api\UserController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\RoleController;
+
+Route::get('/users', [UserController::class, 'index']);
+
+Route::post('/register', [AuthController::class, 'register']);     // 1.1
+Route::post('/login', [AuthController::class, 'login']);           // 1.2
+Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+
+Route::post('/forgot-password', [AuthController::class, 'forgot']); // 1.5
+Route::post('/reset-password', [AuthController::class, 'reset']);   // 1.5
+
+
+
+// thanh có người ye
