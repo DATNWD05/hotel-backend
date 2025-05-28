915e10bf8eb65d50a0d79e17f2c60d93bc5556d7
VuThanhLong05 <longvtph49910@gmail.com>
2025-05-28 21:57:18 +0700

    Fix Khóa ngoại

--- database/migrations/2025_05_27_073425_create_rooms_table.php
+++ database/migrations/2025_05_27_073425_create_rooms_table.php
@@ -10,15 +10,19 @@ class CreateRoomsTable extends Migration
     {
         Schema::create('rooms', function (Blueprint $table) {
             $table->id();
-            $table->string('room_number');      // Số phòng (101, 102, ...)
-            $table->foreignId('room_type_id')->constrained()->onDelete('cascade');  // Liên kết với bảng room_types
-            $table->foreignId('floor_id')->constrained()->onDelete('cascade');     // Liên kết với bảng floors
-            $table->decimal('price', 10, 2);    // Giá phòng
-            $table->enum('status', ['available', 'booked', 'cleaning', 'maintenance'])->default('available');  // Trạng thái phòng
-            $table->string('image')->nullable();  // Thêm trường `image` với kiểu dữ liệu string để lưu đường dẫn ảnh
+            $table->string('room_number'); // Số phòng (101, 102, ...)
+
+            // Khóa ngoại có thể null
+            $table->foreignId('room_type_id')->nullable()->constrained()->nullOnDelete();
+            $table->foreignId('floor_id')->nullable()->constrained()->nullOnDelete();
+
+            $table->decimal('price', 10, 2); // Giá phòng
+            $table->enum('status', ['available', 'booked', 'cleaning', 'maintenance'])->default('available'); // Trạng thái phòng
+            $table->string('image')->nullable(); // Đường dẫn ảnh
             $table->timestamps();
         });
     }
+
 
     public function down()
     {
