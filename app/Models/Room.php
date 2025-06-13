ff4c0d820cd11bee94d857c32c21dbe1ee0bc413
VuThanhLong05 <longvtph49910@gmail.com>
2025-06-13 18:40:23 +0700

    Xóa xử lý giá

--- app/Models/Room.php
+++ app/Models/Room.php
@@ -4,19 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use App\Models\RoomType;
-use App\Models\Floor;
+// use App\Models\Floor;
 
 class Room extends Model
 {
-    protected $fillable = ['room_number', 'room_type_id', 'floor_id', 'price', 'status', 'image'];
+    protected $fillable = ['room_number', 'room_type_id', 'status', 'image'];
 
     public function roomType()
     {
         return $this->belongsTo(RoomType::class);  // Mối quan hệ với loại phòng
     }
 
-    public function floor()
-    {
-        return $this->belongsTo(Floor::class);    // Mối quan hệ với tầng
-    }
+    // public function floor()
+    // {
+    //     return $this->belongsTo(Floor::class);    // Mối quan hệ với tầng
+    // }
 }
