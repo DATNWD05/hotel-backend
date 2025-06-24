65febef6df09110e7419dce289e262c8e61945b5
Thanh <thanh1705aurai@gmail.com>
2025-06-24 22:58:30 +0700

    sửa lại biến mất bảng departments và employees

---
+++ database/migrations/2025_05_28_161312_create_employees_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up()
+    {
+        Schema::create('employees', function (Blueprint $table) {
+    $table->id();
+
+    $table->string('MaNV')->unique();
+    $table->string('name');
+    $table->date('birthday')->nullable();
+    $table->string('email');
+    $table->string('gender')->nullable();
+    $table->string('phone')->nullable();
+    $table->string('address')->nullable();
+    $table->date('hire_date')->nullable();
+
+    $table->unsignedBigInteger('role_id')->nullable();
+    $table->foreign('role_id')->references('id')->on('roles')->onDelete('set null');
+
+    $table->unsignedBigInteger('user_id')->unique();
+    $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+
+    $table->unsignedBigInteger('department_id')->nullable();
+    $table->foreign('department_id')->references('id')->on('departments')->onDelete('set null');
+
+    $table->string('status')->default('active');
+    $table->string('cccd')->nullable();
+
+    $table->timestamps();
+});
+
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('employees');
+    }
+};
