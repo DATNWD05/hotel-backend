4631d60b9c398d82492b1bc5389eec79848a92fd
THAO <thaovttph50036@gmail.com>
2025-07-13 14:07:37 +0700

    fix phan quyen season4

--- app/Policies/BookingPolicy.php
+++ app/Policies/BookingPolicy.php
@@ -12,4 +12,33 @@ class BookingPolicy extends BasePolicy
     {
         return $this->check($user, 'cancel_bookings');
     }
+    public function checkIn(User $user, Booking $booking)
+    {
+        return $this->check($user, 'checkin_bookings');
+    }
+
+    public function checkOut(User $user, Booking $booking)
+    {
+        return $this->check($user, 'checkout_bookings');
+    }
+
+    public function payDeposit(User $user, Booking $booking)
+    {
+        return $this->check($user, 'pay_deposit');
+    }
+
+    public function addServices(User $user, Booking $booking)
+    {
+        return $this->check($user, 'add_services');
+    }
+
+    public function removeService(User $user, Booking $booking)
+    {
+        return $this->check($user, 'remove_services');
+    }
+
+    public function payByCash(User $user, Booking $booking)
+    {
+        return $this->check($user, 'pay_by_cash');
+    }
 }
